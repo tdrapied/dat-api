20d3ad3e08b61aea27bcf82bf9d82cb94b18961b
tdrapied <t.drapied@gmail.com>
2022-07-11 21:11:10 +0200

    Add user permissions

--- src/modules/locations/locations.service.ts
+++ src/modules/locations/locations.service.ts
@@ -2,16 +2,19 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Location } from './entities/location.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class LocationsService {
   constructor(
     @InjectRepository(Location)
     private locationRepository: Repository<Location>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
-  findAll(): Promise<Location[]> {
-    return this.locationRepository.find();
+  findAll(currentUser: User): Location[] {
+    return currentUser.locations;
   }
 
   /********************************************
